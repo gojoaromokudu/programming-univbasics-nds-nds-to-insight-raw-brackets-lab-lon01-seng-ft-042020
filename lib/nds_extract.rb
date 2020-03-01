0f45f6744f25d90d778379e142ed0b6de58fd3e0
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-01 19:31:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,18 +17,13 @@ def directors_totals(nds)
     column_index = 0
     while column_index < nds[row_index][:movies].length
     puts nds[row_index][:movies][column_index][:worldwide_gross]
-   #i = 0
-   # while i < 5
     director_sum = 0 
     director_sum += nds[row_index][:movies][column_index][:worldwide_gross]
-    
     column_index += 1 
- # i += 1
 end
- # end
   result[nds[row_index][:name]] = director_sum
     row_index += 1
   end
-# puts nds[row_index].length
+ puts nds[row_index].length
  result
 end
